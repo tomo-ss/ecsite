14f1cbeb76a36fe223498958964d1c6197582416
tomo-ss <mssp.0321@outlook.jp>
2025-07-18 21:51:09 +0900

    login.jsp写経完了

---
+++ ecsite/src/com/diworksdev/ecsite/action/LoginAction.java
@@ -0,0 +1,59 @@
+package com.diworksdev.ecsite.action;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport implements SessionAware {
+
+	private String loginUserId;
+	private String loginPassword;
+	private Map<String, Object> session;
+
+	private LoginDAO loginDAO = new LoginDAO();
+	private BuyItemDAO buyItemDAO = new BuyItemDAO();
+
+	@Override
+	public String execute() {
+		String result = ERROR;
+
+		LoginDTO loginDTO = loginDAO.getLoginUserInfo(loginUserId, loginPassword);
+		session.put("loginUser", loginDTO);
+
+		if (loginDTO.getLoginFlg()) {
+			result = SUCCESS;
+
+			BuyItemDTO buyItemDTO = buyItemDAO.getBuyItemInfo();
+
+			session.put("login_user_id", loginDTO.getLoginId());
+			session.put("id", buyItemDTO.getId());
+			session.put("buyItem_name", buyItemDTO.getItemName());
+			session.put("buyItem_price", buyItemDTO.getItemPrice());
+		}
+
+		return result;
+	}
+
+	public String getLoginUserId() {
+		return loginUserId;
+	}
+
+	public void setLoginUserId(String loginUserId) {
+		this.loginUserId = loginUserId;
+	}
+
+	public String getLoginPassword() {
+		return loginPassword;
+	}
+
+	public void setLoginPassword(String loginPassword) {
+		this.loginPassword = loginPassword;
+	}
+
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+}
